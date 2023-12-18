af2187d0b78012e2a6d9b4647ffd41bda574ca87
bigzinxb <bigzin369@gmail.com>
2023-12-18 16:25:59 -0300

    first commit

---
+++ lib/bitz.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bitz/version"
+
+module Bitz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/bitz_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Bitz do
+  it "has a version number" do
+    expect(Bitz::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
