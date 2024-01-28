807e6dca9b8d1d188801767507fe27d620e864b7
bigzinxb <bigzin369@gmail.com>
2024-01-28 18:54:36 -0300

    ...

--- lib/fixed_functions_bits.rb
+++ lib/fixed_functions_bits.rb
@@ -3,28 +3,6 @@ module FixedFunctionsBits
         str.reverse
     end
 
-    def self.padding_to_mathematical_operations!(product_bits_from_above, bits_of_product_from_below)
-        if product_bits_from_above.length < bits_of_product_from_below.length
-            until product_bits_from_above.length == bits_of_product_from_below.length
-                queue(product_bits_from_above)
-            end
-        end
-
-        if product_bits_from_above.length > bits_of_product_from_below.length
-            until product_bits_from_above.length == bits_of_product_from_below.length
-                queue(bits_of_product_from_below)
-            end
-        end
-    end
-
-    def padding(str, amount_zero)
-        while amount_zero >= str.length
-            queue(str)
-        end
-
-        return str
-    end
-
     private
     def self.queue(str)
         str.insert(0, "0")
